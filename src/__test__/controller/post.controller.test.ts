f7c3b3c58c31966b2429bbc9dc882eea20f370ed
Tejkumar Jain <tejkumar.2103@gmail.com>
2024-02-11 15:40:28 +0530

    adding testing environment

---
+++ src/__test__/controller/post.controller.test.ts
@@ -0,0 +1,7 @@
+describe("Testing PostController",  () =>
+{
+    it("testing first app", () => 
+    {
+        expect(true).toBe(true);
+    })
+})
---
+++ src/__test__/index.test.ts
@@ -0,0 +1,7 @@
+describe("Testing",  () =>
+{
+    it("testing first app", () => 
+    {
+        expect(true).toBe(true);
+    })
+})
